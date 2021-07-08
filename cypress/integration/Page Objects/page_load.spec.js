1aee8e0ba8bbd4c939be9e2ce97ac219ba7c046b
Manu Nair <mnair@themxgroup.com>
2021-07-08 10:15:49 -0500

    Updated visits to requests to speed up test
    Visual loads aren't necessary for this test

--- cypress/integration/Page Objects/page_load.spec.js
+++ cypress/integration/Page Objects/page_load.spec.js
@@ -1,9 +1,11 @@
 //https://www.toolsqa.com/cypress/page-object-pattern-in-cypress/
-//Updatable URL list. Must be same domain, with https at start. Test can be duplicated for various domains.
+//Updatable URL list.
+//Must be same domain, with https at start. 
+//This test can be duplicated for various domains.
 const urls = [
     'https://www.cypress.io',
     'https://www.cypress.io/support',
-    'https://www.cypress.io/blog',
+    'https://manu.cypress.io',
 ];
 
 const urls2 = [
@@ -15,13 +17,13 @@ const urls2 = [
 class PageLoad {
     loadPages() {
         urls.forEach((url) => {
-              cy.visit(url)
+              cy.request(url)
               cy.log(url + ' loaded successfully')
           })
     }
     loadPages2() {
         urls2.forEach((url) => {
-              cy.visit(url)
+              cy.request(url)
               cy.log(url + ' loaded successfully')
           })
     }
